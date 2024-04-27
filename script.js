29de172878f4e3edba2283f730d93f24bbb72f0e
Aosegir <128326345+Aosegir@users.noreply.github.com>
2024-04-26 20:10:38 -0700

    Created files and related them to HTML. Created temporary grid which showcases 9 tic-tac-toe tiles and allows for the background to change color on hover.

---
+++ script.js
@@ -0,0 +1,11 @@
+/*
+    fuck the suggestions start with the html
+    just make a grid??
+    grid and column tracks can serve as lines for the thing
+    let user click and make an X
+
+
+
+
+
+*/
